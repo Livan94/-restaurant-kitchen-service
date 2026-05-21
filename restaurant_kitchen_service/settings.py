c18cda2b6074e42887036836e0d7aab7566f0018
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-05-21 22:49:11 +0300

    chore: configure environment variables with python-decouple

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6s(%39+yoj@%f5vmgrg0xpge=v%2mk)e-=)i5^7rl+9@eqhwpc'
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
