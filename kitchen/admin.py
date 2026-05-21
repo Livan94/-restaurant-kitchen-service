b491a2015644300968ca78507fd4130f2cd28de5
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-05-21 18:54:58 +0300

    feat: register models in admin panel with customized fields

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Cook, DishType, Ingredient, Dish
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": ("years_of_experience",)
+            }
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": ("years_of_experience",)
+            }
+        ),
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "dish_type")
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+    filter_horizontal = ("cooks", "ingredients")
