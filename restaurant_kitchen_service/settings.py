ecf3a351b11412449fbc1a060f2327d247356dca
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-05-21 23:05:07 +0300

    feat: add django-debug-toolbar for development

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
+
     'kitchen',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,9 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# Auth
 AUTH_USER_MODEL = 'kitchen.Cook'
-
 # LOGIN_REDIRECT_URL = "/"
 # LOGOUT_REDIRECT_URL = "/accounts/login/"
 # LOGIN_URL = "/accounts/login/"
+
+# Debug Toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),  # login/logout
     path("", include("kitchen.urls", namespace="kitchen")),
-]
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
