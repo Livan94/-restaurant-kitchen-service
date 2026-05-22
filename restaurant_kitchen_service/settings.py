60b24020780c5e9ca3a5fa43f67e347158c9a8c2
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-05-22 16:26:57 +0300

    chore: add and configure django-crispy-forms with bootstrap5 pack

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -30,7 +29,6 @@ ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"] # for .env.DEBUG=False
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "debug_toolbar",
+
+    "crispy_forms",
+    "crispy_bootstrap5",
 
     'kitchen',
 ]
@@ -76,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restaurant_kitchen_service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
@@ -86,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -106,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -117,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
@@ -135,3 +132,6 @@ LOGIN_URL = "login"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
