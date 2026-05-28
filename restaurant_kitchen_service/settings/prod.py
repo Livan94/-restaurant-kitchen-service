ff03ffa78a4c302d7c07ca54878578c05af9303e
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-05-28 16:20:08 +0300

    chore: add Render external hostname to ALLOWED_HOSTS

--- restaurant_kitchen_service/settings/prod.py
+++ restaurant_kitchen_service/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from decouple import config, Csv  # noqa: F401
 
 from restaurant_kitchen_service.settings.base import *  # noqa: F403,F401
@@ -10,6 +12,10 @@ ALLOWED_HOSTS = config(
     default="127.0.0.1,localhost",
     cast=Csv()
 )
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
